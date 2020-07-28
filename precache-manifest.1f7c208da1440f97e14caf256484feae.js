417b00885dc5cb316eed40a2ef44af1410acfb46
quincyhill <therealquincyhill@gmail.com>
2020-07-28 15:23:10 -0500

    Updates

---
+++ precache-manifest.1f7c208da1440f97e14caf256484feae.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "564006cf9b3df8da1ee2d8754c9520de",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f6267e486c7f0150459c",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "976ca09514e388a1301c",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "f6267e486c7f0150459c",
+    "url": "/static/js/2.0a3075ef.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.0a3075ef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "976ca09514e388a1301c",
+    "url": "/static/js/main.778368ab.chunk.js"
+  },
+  {
+    "revision": "7b7ce108df4adaf35215",
+    "url": "/static/js/runtime-main.af20599d.js"
+  },
+  {
+    "revision": "9607cacb7900c04a5a4473426db8ed27",
+    "url": "/static/media/test.9607cacb.svg"
+  }
+]);
--- src/themes/colors.ts
+++
@@ -1,2 +0,0 @@
-export const primaryColor: string = "#ffa500";
-const backupPrimaryColor: string = "#2a9df4";
