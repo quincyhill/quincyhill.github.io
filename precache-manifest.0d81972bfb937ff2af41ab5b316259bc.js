8024d3d12683f73cb11d8230d6ce18eeee22138e
quincyhill <therealquincyhill@gmail.com>
2021-04-26 23:12:20 -0500

    Updates

---
+++ precache-manifest.0d81972bfb937ff2af41ab5b316259bc.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4505dc6022917d3eb1515f41cf8b8503",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7f0b6556824af68c8656",
+    "url": "/static/css/main.507877fb.chunk.css"
+  },
+  {
+    "revision": "0a538fcdc5ad41c0182a",
+    "url": "/static/js/2.2e284db9.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.2e284db9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7f0b6556824af68c8656",
+    "url": "/static/js/main.8b142335.chunk.js"
+  },
+  {
+    "revision": "7b7ce108df4adaf35215",
+    "url": "/static/js/runtime-main.af20599d.js"
+  },
+  {
+    "revision": "21ffab1ea970ad750663a6bf9ccb4f0d",
+    "url": "/static/media/moon.21ffab1e.svg"
+  },
+  {
+    "revision": "034ffb44ddc3f87bda36facf0e6f199c",
+    "url": "/static/media/sun.034ffb44.svg"
+  }
+]);
