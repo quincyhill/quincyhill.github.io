cb06d09f5909b09d17e4d3423a292f2b5eefbbc1
quincyhill <therealquincyhill@gmail.com>
2021-11-18 18:21:01 -0600

    Refactored svg's, fixed up alot of css issues with transform, condensed javascript to make sane use of classes, still needs some additional work with the header on mobile but for another time

--- js/app.js
+++ js/app.js
@@ -1,133 +1,101 @@
-/* Theme switcher */
+// Theme Switcher
 const theme_btn = document.getElementById('theme-switcher')
-const sun_img = document.getElementById('sun-img')
-const moon_img = document.getElementById('moon-img')
+const sun_div = document.getElementById('sun-div')
+const moon_div = document.getElementById('moon-div')
 
-/* Buttons */
-const goals_btn = document.getElementById('goals-btn')
-const education_btn = document.getElementById('education-btn')
-const projects_btn = document.getElementById('projects-btn')
-const skills_btn = document.getElementById('skills-btn')
-
-/* Items */
-const goals_item = document.getElementById('goals-item')
-const eduction_item = document.getElementById('education-item')
-const projects_item = document.getElementById('projects-item')
-const skills_item = document.getElementById('skills-item')
-
+// Theme tokens
 const light_theme = 'light-theme'
 const dark_theme = 'dark-theme'
 const theme_token = 'quincyhill.xyz:theme'
-
-let goals_is_open = false
-let education_is_open = false
-let projects_is_open = false
-let skills_is_open = false
+let g_current_theme = localStorage.getItem(theme_token)
 
 function toggle_theme() {
-	switch (localStorage.getItem(theme_token)) {
-		case light_theme:
-			/* switch to dark theme if its already light */
-			localStorage.setItem(theme_token, dark_theme)
-			document.body.className = dark_theme
-			sun_img.setAttribute('hidden', 'true')
-			moon_img.removeAttribute('hidden')
-			break
-		case dark_theme:
-			/* switch to light theme if its already dark*/
-			localStorage.setItem(theme_token, light_theme)
-			document.body.className = light_theme
-			moon_img.setAttribute('hidden', 'true')
-			sun_img.removeAttribute('hidden')
-			break
-		default:
-			/* In some strange event that the value is corrupted, log to output */
-			console.log('The theme choice is invalid!')
-			break
-	}
-}
-
-function toggle_goals() {
-	switch (goals_is_open) {
-		case true:
-			goals_is_open = false
-			goals_item.setAttribute('hidden', 'true')
-			break
-		case false:
-			goals_is_open = true
-			goals_item.removeAttribute('hidden')
-			goals_item.setAttribute('display', 'flex')
-			break
-	}
-}
-
-function toggle_education() {
-	switch (education_is_open) {
-		case true:
-			education_is_open = false
-			eduction_item.setAttribute('hidden', 'true')
-			break
-		case false:
-			education_is_open = true
-			eduction_item.removeAttribute('hidden')
-			eduction_item.setAttribute('display', 'flex')
-			break
-	}
-}
-
-function toggle_projects() {
-	switch (projects_is_open) {
-		case true:
-			projects_is_open = false
-			projects_item.setAttribute('hidden', 'true')
-			break
-		case false:
-			projects_is_open = true
-			projects_item.removeAttribute('hidden')
-			projects_item.setAttribute('display', 'flex')
-			break
-	}
-}
-
-function toggle_skills() {
-	switch (skills_is_open) {
-		case true:
-			skills_is_open = false
-			skills_item.setAttribute('hidden', 'true')
-			break
-		case false:
-			skills_is_open = true
-			skills_item.removeAttribute('hidden')
-			skills_item.setAttribute('display', 'flex')
-			break
-	}
-}
-
-/* Makes an intial check to see if site theme token exists */
-switch (localStorage.getItem(theme_token)) {
-	case light_theme:
-		document.body.className = light_theme
-		moon_img.setAttribute('hidden', 'true')
-		sun_img.removeAttribute('hidden')
-		break
-	case dark_theme:
+	const current_theme = localStorage.getItem(theme_token)
+	if (current_theme === light_theme) {
+		localStorage.setItem(theme_token, dark_theme)
 		document.body.className = dark_theme
-		sun_img.setAttribute('hidden', 'true')
-		moon_img.removeAttribute('hidden')
-		break
-	default:
-		/* generates default light theme token if none exist */
+		sun_div.setAttribute('hidden', 'true')
+		moon_div.removeAttribute('hidden')
+	} else if (current_theme === dark_theme) {
 		localStorage.setItem(theme_token, light_theme)
 		document.body.className = light_theme
-		moon_img.setAttribute('hidden', 'true')
-		sun_img.removeAttribute('hidden')
+		moon_div.setAttribute('hidden', 'true')
+		sun_div.removeAttribute('hidden')
+	} else {
+		console.log('The theme choice is invalid!, defaulting to light theme ')
+		localStorage.setItem(theme_token, light_theme)
+		document.body.className = light_theme
+		moon_div.setAttribute('hidden', 'true')
+		sun_div.removeAttribute('hidden')
+	}
 }
 
-/* Theme */
+class ToggleDropdown {
+	constructor(button_name, item_name) {
+		this.dropdown_btn = document.getElementById(button_name)
+		this.dropdown_item = document.getElementById(item_name)
+		this.is_open = false
+	}
+
+	toggle_item() {
+		if (this.is_open) {
+			this.dropdown_item.setAttribute('hidden', 'true')
+			this.is_open = false
+		} else if (!this.is_open) {
+			this.dropdown_item.removeAttribute('hidden')
+			this.dropdown_item.setAttribute('display', 'flex')
+			this.is_open = true
+		} else {
+			console.log('value is neither true or false')
+		}
+	}
+
+	change_button() {
+		this.dropdown_btn.innerHTML = 'hi'
+	}
+
+	run_event() {
+		this.dropdown_btn.addEventListener(
+			'click',
+			() => {
+				this.toggle_item()
+			},
+			false
+		)
+	}
+}
+
+// Create objects for the dropdown toggle
+let goals_dropdown = new ToggleDropdown('goals-btn', 'goals-item')
+let education_dropdown = new ToggleDropdown('education-btn', 'education-item')
+let projects_dropdown = new ToggleDropdown('projects-btn', 'projects-item')
+let skills_dropdown = new ToggleDropdown('skills-btn', 'skills-item')
+let contact_dropdown = new ToggleDropdown('contact-btn', 'contact-item')
+
+// Makes an intial check to see if site theme token exists
+
+if (g_current_theme === light_theme) {
+	document.body.className = light_theme
+	moon_div.setAttribute('hidden', 'true')
+	sun_div.removeAttribute('hidden')
+} else if (g_current_theme === dark_theme) {
+	document.body.className = dark_theme
+	sun_div.setAttribute('hidden', 'true')
+	moon_div.removeAttribute('hidden')
+} else {
+	// Generates default light token
+	localStorage.setItem(theme_token, light_theme)
+	document.body.className = light_theme
+	moon_div.setAttribute('hidden', 'true')
+	sun_div.removeAttribute('hidden')
+}
+
+// Theme
 theme_btn.addEventListener('click', toggle_theme, false)
 
-/* Buttons*/
-goals_btn.addEventListener('click', toggle_goals, false)
-education_btn.addEventListener('click', toggle_education, false)
-projects_btn.addEventListener('click', toggle_projects, false)
-skills_btn.addEventListener('click', toggle_skills, false)
+// Run the events from the drop down objects
+goals_dropdown.run_event()
+education_dropdown.run_event()
+projects_dropdown.run_event()
+skills_dropdown.run_event()
+contact_dropdown.run_event()
