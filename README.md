f691243030f789d1f46355b6d8834ec61c144af8
quincyhill <therealquincyhill@gmail.com>
2021-11-24 01:06:29 -0600

    trying some different color schemes, removed the class from js and expanding its use

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # My Portfolio
 Come check it out
+
+# Notes:
+I've noticed some weird glitching in the browser if its updated and the cache isnt reset, eg ctrl + f5.
+I'm going to add some js to detect a version change then force a recache if thats the case
+So far this issue just is on firefox, the mobile browser doesn't have this issue
+Caching issue seems to be just on fire fox
+
+* NoScript
+Make a non javascript version of the site that doesnt include any of the funtionality just displays info
--- js/app.js
+++ js/app.js
@@ -9,68 +9,114 @@ const dark_theme = 'dark-theme'
 const theme_token = 'quincyhill.xyz:theme'
 let g_current_theme = localStorage.getItem(theme_token)
 
+// Other Div
+const main_div = document.getElementById('main-div')
+
+// Colors
+const light_color_lighter = getComputedStyle(
+	document.documentElement
+).getPropertyValue('--light-color-lighter')
+const light_color_darker = getComputedStyle(
+	document.documentElement
+).getPropertyValue('--light-color-darker')
+const dark_color_lighter = getComputedStyle(
+	document.documentElement
+).getPropertyValue('--dark-color-lighter')
+const dark_color_darker = getComputedStyle(
+	document.documentElement
+).getPropertyValue('--dark-color-darker')
+
 function toggle_theme() {
-	const current_theme = localStorage.getItem(theme_token)
-	if (current_theme === light_theme) {
+	if (g_current_theme === light_theme) {
 		localStorage.setItem(theme_token, dark_theme)
+		g_current_theme = dark_theme
 		document.body.className = dark_theme
 		sun_div.setAttribute('hidden', 'true')
 		moon_div.removeAttribute('hidden')
-	} else if (current_theme === dark_theme) {
+	} else if (g_current_theme === dark_theme) {
 		localStorage.setItem(theme_token, light_theme)
+		g_current_theme = light_theme
 		document.body.className = light_theme
 		moon_div.setAttribute('hidden', 'true')
 		sun_div.removeAttribute('hidden')
 	} else {
 		console.log('The theme choice is invalid!, defaulting to light theme ')
 		localStorage.setItem(theme_token, light_theme)
+		g_current_theme = light_theme
 		document.body.className = light_theme
 		moon_div.setAttribute('hidden', 'true')
 		sun_div.removeAttribute('hidden')
 	}
 }
 
-class ToggleDropdown {
-	constructor(button_name, item_name) {
-		this.dropdown_btn = document.getElementById(button_name)
-		this.dropdown_item = document.getElementById(item_name)
-		this.is_open = false
-	}
-
-	toggle_item() {
-		if (this.is_open) {
-			this.dropdown_item.setAttribute('hidden', 'true')
-			this.is_open = false
-		} else if (!this.is_open) {
-			this.dropdown_item.removeAttribute('hidden')
-			this.dropdown_item.setAttribute('display', 'flex')
-			this.is_open = true
-		} else {
-			console.log('value is neither true or false')
-		}
-	}
-
-	change_button() {
-		this.dropdown_btn.innerHTML = 'hi'
-	}
-
-	run_event() {
-		this.dropdown_btn.addEventListener(
-			'click',
-			() => {
-				this.toggle_item()
-			},
-			false
-		)
-	}
+function gradient_move_mouse(ev, first_color, second_color) {
+	let mouseXpercentage = Math.round((ev.pageX / window.innerWidth) * 100)
+	let mouseYpercentage = Math.round((ev.pageY / window.innerHeight) * 100)
+	let background_text = `radial-gradient(at ${mouseXpercentage}% ${mouseYpercentage}%, ${first_color}, ${second_color})`
+	main_div.style.background = background_text
 }
 
 // Create objects for the dropdown toggle
-let goals_dropdown = new ToggleDropdown('goals-btn', 'goals-item')
-let education_dropdown = new ToggleDropdown('education-btn', 'education-item')
-let projects_dropdown = new ToggleDropdown('projects-btn', 'projects-item')
-let skills_dropdown = new ToggleDropdown('skills-btn', 'skills-item')
-let contact_dropdown = new ToggleDropdown('contact-btn', 'contact-item')
+const goals_dropdown = {
+	dropdown_btn: document.getElementById('goals-btn'),
+	dropdown_item: document.getElementById('goals-item'),
+	is_open: false,
+}
+
+const education_dropdown = {
+	dropdown_btn: document.getElementById('education-btn'),
+	dropdown_item: document.getElementById('education-item'),
+	is_open: false,
+}
+
+const projects_dropdown = {
+	dropdown_btn: document.getElementById('projects-btn'),
+	dropdown_item: document.getElementById('projects-item'),
+	is_open: false,
+}
+
+const skills_dropdown = {
+	dropdown_btn: document.getElementById('skills-btn'),
+	dropdown_item: document.getElementById('skills-item'),
+	is_open: false,
+}
+
+const contact_dropdown = {
+	dropdown_btn: document.getElementById('contact-btn'),
+	dropdown_item: document.getElementById('contact-item'),
+	is_open: false,
+}
+
+const experience_dropdown = {
+	dropdown_btn: document.getElementById('experience-btn'),
+	dropdown_item: document.getElementById('experience-item'),
+	is_open: false,
+}
+
+function toggle_dropdown(dropdown) {
+	if (dropdown.is_open) {
+		dropdown.dropdown_item.setAttribute('hidden', 'true')
+		dropdown.dropdown_item.classList.remove('show-drop-down')
+		dropdown.is_open = false
+	} else if (!dropdown.is_open) {
+		dropdown.dropdown_item.classList.add('show-drop-down')
+		dropdown.dropdown_item.removeAttribute('hidden')
+		dropdown.dropdown_item.setAttribute('display', 'flex')
+		dropdown.is_open = true
+	} else {
+		console.log('error with dropdown is open value')
+	}
+}
+
+function handle_dropdown(dropdown) {
+	dropdown.dropdown_btn.addEventListener(
+		'click',
+		() => {
+			toggle_dropdown(dropdown)
+		},
+		false
+	)
+}
 
 // Makes an intial check to see if site theme token exists
 
@@ -86,16 +132,45 @@ if (g_current_theme === light_theme) {
 	// Generates default light token
 	localStorage.setItem(theme_token, light_theme)
 	document.body.className = light_theme
+	g_current_theme = light_theme
 	moon_div.setAttribute('hidden', 'true')
 	sun_div.removeAttribute('hidden')
 }
 
-// Theme
-theme_btn.addEventListener('click', toggle_theme, false)
+// Dropdown handling
+handle_dropdown(goals_dropdown)
+handle_dropdown(education_dropdown)
+handle_dropdown(projects_dropdown)
+handle_dropdown(experience_dropdown)
+handle_dropdown(skills_dropdown)
+handle_dropdown(contact_dropdown)
 
-// Run the events from the drop down objects
-goals_dropdown.run_event()
-education_dropdown.run_event()
-projects_dropdown.run_event()
-skills_dropdown.run_event()
-contact_dropdown.run_event()
+experience_dropdown.dropdown_btn.addEventListener('click', () => {})
+
+// Theme handling
+theme_btn.addEventListener(
+	'click',
+	(ev) => {
+		toggle_theme()
+		if (g_current_theme === light_theme) {
+			main_div.style.background = `radial-gradient(at center, ${light_color_lighter}, ${light_color_darker})`
+		} else if (g_current_theme === dark_theme) {
+			main_div.style.background = `radial-gradient(at center, ${dark_color_lighter}, ${dark_color_darker})`
+		}
+		console.log(g_current_theme)
+	},
+	false
+)
+
+// Radial background handling
+main_div.addEventListener(
+	'mousemove',
+	(ev) => {
+		if (g_current_theme === light_theme) {
+			gradient_move_mouse(ev, light_color_lighter, light_color_darker)
+		} else {
+			gradient_move_mouse(ev, dark_color_lighter, dark_color_darker)
+		}
+	},
+	false
+)
